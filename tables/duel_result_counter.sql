e418c88681fc1feb3d7923e09faf2936cddfb32f
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-09-07 05:32:42 -0500

    chore: Edit duel_result_counter table

--- tables/duel_result_counter.sql
+++ tables/duel_result_counter.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS public.duel_result_counter (
-    total          integer NOT NULL DEFAULT 0,
-    wins           integer NOT NULL DEFAULT 0,
-    losses         integer NOT NULL DEFAULT 0,
-    draws_abandons integer NOT NULL DEFAULT 0
+    total    integer NOT NULL DEFAULT 0,
+    wins     integer NOT NULL DEFAULT 0,
+    losses   integer NOT NULL DEFAULT 0,
+    draws    integer NOT NULL DEFAULT 0,
+    abandons integer NOT NULL DEFAULT 0
 );
 
 ALTER TABLE public.duel_result_counter ENABLE ROW LEVEL SECURITY;
