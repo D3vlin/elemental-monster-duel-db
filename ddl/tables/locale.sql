18b9600fa232859f4d5ad80990f055b99f2ab431
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-09-25 11:55:35 -0500

    chore: Edit locale

--- ddl/tables/locale.sql
+++ ddl/tables/locale.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.locale (
-    code varchar(5) PRIMARY KEY
+    id   bigserial   PRIMARY KEY,
+    code varchar(5)  NOT NULL UNIQUE
 );
 
 ALTER TABLE public.locale ENABLE ROW LEVEL SECURITY;
