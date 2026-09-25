d9391536a1b680bfb9b06416379e51a84f167409
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-09-25 13:03:38 -0500

    chore: Add power_rank

--- ddl/tables/power_rank_translation.sql
+++ ddl/tables/power_rank_translation.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS public.power_rank_translation (
     power_rank varchar(32) NOT NULL,
-    locale     varchar(5)  NOT NULL,
+    locale_id  bigint      NOT NULL,
     label      varchar(40) NOT NULL,
 
-    CONSTRAINT pk_power_rank_translation PRIMARY KEY (power_rank, locale),
+    CONSTRAINT pk_power_rank_translation PRIMARY KEY (power_rank, locale_id),
     CONSTRAINT fk_power_rank_translation_power_rank FOREIGN KEY (power_rank) REFERENCES public.power_rank (code),
-    CONSTRAINT fk_power_rank_translation_locale FOREIGN KEY (locale) REFERENCES public.locale (code)
+    CONSTRAINT fk_power_rank_translation_locale FOREIGN KEY (locale_id) REFERENCES public.locale (id)
 );
 
 ALTER TABLE public.power_rank_translation ENABLE ROW LEVEL SECURITY;
